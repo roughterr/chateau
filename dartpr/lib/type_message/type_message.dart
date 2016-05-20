d7da886788307996376b99f7e3256781ccf7f991
roughterr <roughterr@gmail.com>
2016-05-20 16:41:33 +0300

    now Ctrl+Enter pastes a new symbol character and a simle Enter initiates the submit

--- dartpr/lib/type_message/type_message.dart
+++ dartpr/lib/type_message/type_message.dart
@@ -13,24 +13,44 @@ import 'package:polymer_elements/paper_textarea.dart';
 /// This Polymer element will contain an input fiend and a button "Send".
 @PolymerRegister('type-message')
 class TypeMessage extends PolymerElement {
-  @property
-  String text;
+  PaperTextarea textArea;
+
+  /// This variable indicated that the last pressed key was Enter.
+  bool _lastKeyWasEnter;
 
   TypeMessage.created() : super.created();
+
+  void ready() {
+    textArea = this.querySelector("paper-textarea");
+  }
 
   /// This method is called when the button "Send" is pressed.
   @reflectable
   void sendPressed(event, [_]) {
-    print("Send pressed. text is ${text}");
+    print("Send pressed. textArea.value is ${textArea.value}");
+    textArea.value = null;
   }
 
   /// This method is called when the button "Send" is pressed.
   @reflectable
   void inputKeyPressed(CustomEventWrapper event, [_]) {
+    _lastKeyWasEnter = false;
     final KeyboardEvent keyboardEvent = event.original as KeyboardEvent;
-    // If a Ctrl and Enter buttons were pressed simultateously.
-    if (keyboardEvent.ctrlKey &&
-        (keyboardEvent.keyCode == 13 || keyboardEvent.keyCode == 10)) {
+    // If a Ctrl and Enter buttons were pressed simultaneously.
+    if (keyboardEvent.keyCode == 13 || keyboardEvent.keyCode == 10) {
+      if (keyboardEvent.ctrlKey) {
+        // Ctrl+Enter combination transforms into a next line symbol.
+        textArea.value = textArea.value + "\n";
+      } else {
+        _lastKeyWasEnter = true;
+      }
+    }
+  }
+
+  /// This method is the user types something in the input text area.
+  @reflectable
+  void inputInput(CustomEventWrapper event, [_]) {
+    if (_lastKeyWasEnter) {
       sendPressed(null);
     }
   }
