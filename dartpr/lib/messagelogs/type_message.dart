25709756cca9daa50236815603aa753e1c75fab7
roughterr <roughterr@gmail.com>
2016-05-20 13:04:59 +0300

    added a text area with multiple rows

--- dartpr/lib/messagelogs/type_message.dart
+++ dartpr/lib/messagelogs/type_message.dart
@@ -4,14 +4,15 @@
 @HtmlImport('type_message.html')
 library dartpr.lib.type_message;
 
+import 'dart:html';
+
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart';
 import 'package:polymer_elements/paper_button.dart';
-import 'package:polymer_elements/paper_input.dart';
-import 'package:polymer_elements/paper_input.html';
+import 'package:polymer_elements/paper_textarea.dart';
 
 /// Uses [PaperButton]
-/// /// Uses [PaperInput]
+/// Uses [PaperTextarea]
 @PolymerRegister('type-message')
 class TypeMessage extends PolymerElement {
   @property
@@ -23,5 +24,16 @@ class TypeMessage extends PolymerElement {
   @reflectable
   void sendPressed(event, [_]) {
     print("Send pressed. text is ${text}");
+  }
+
+  /// This method is called when the button "Send" is pressed.
+  @reflectable
+  void inputKeyPressed(CustomEventWrapper event, [_]) {
+    final KeyboardEvent keyboardEvent = event.original as KeyboardEvent;
+    // If an Enter button was pressed.
+    if (keyboardEvent.ctrlKey &&
+        (keyboardEvent.keyCode == 13 || keyboardEvent.keyCode == 10)) {
+      sendPressed(null);
+    }
   }
 }
