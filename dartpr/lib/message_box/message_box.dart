60c10b21bd1ef21bab693dfc48134d6e465f8d8d
roughterr <roughterr@gmail.com>
2016-05-20 20:10:34 +0300

    Messages now will be sent from the type-message component to the message-box component via streams

--- dartpr/lib/message_box/message_box.dart
+++ dartpr/lib/message_box/message_box.dart
@@ -1,6 +1,8 @@
 @HtmlImport('message_box.html')
 library dartpr.lib.message_box;
 
+import 'dart:async';
+
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart';
 import 'package:dartpr/type_message/type_message.dart';
@@ -9,5 +11,15 @@ import 'package:dartpr/type_message/type_message.dart';
 /// This Polymer element will contain an input fiend and a button "Send".
 @PolymerRegister('message-box')
 class MessageBox extends PolymerElement {
+  /// Stream of messages that the user types.
+  Stream<String> messagestream;
+
+  /// Subscription to a stream of messages that the user types.
+  StreamSubscription _streamSubscription;
+
   MessageBox.created() : super.created();
+
+  void ready() {
+    _streamSubscription = messagestream.listen((s) => print("s: ${s}"));
+  }
 }
--- dartpr/lib/type_message/type_message.dart
+++ dartpr/lib/type_message/type_message.dart
@@ -1,6 +1,7 @@
 @HtmlImport('type_message.html')
 library dartpr.lib.type_message;
 
+import 'dart:async';
 import 'dart:html';
 
 import 'package:polymer/polymer.dart';
@@ -13,22 +14,32 @@ import 'package:polymer_elements/paper_textarea.dart';
 /// This Polymer element will contain an input fiend and a button "Send".
 @PolymerRegister('type-message')
 class TypeMessage extends PolymerElement {
-  PaperTextarea textArea;
+  /// This variable contains text that the user inputs.
+  PaperTextarea _textArea;
 
   /// This variable indicated that the last pressed key was Enter.
   bool _lastKeyWasEnter;
 
+  /// Provides both write and read access to the stream of messages.
+  StreamController<String> _messageStreamController = new StreamController<
+      String>.broadcast();
+
+  /// Provides read access to the stream of messages.
+  @Property(notify: true)
+  Stream<String> messagestream;
+
   TypeMessage.created() : super.created();
 
   void ready() {
-    textArea = this.querySelector("paper-textarea");
+    _textArea = this.querySelector("paper-textarea");
+    set("messagestream", _messageStreamController.stream);
   }
 
   /// This method is called when the button "Send" is pressed.
   @reflectable
   void sendPressed(event, [_]) {
-    print("Send pressed. textArea.value is ${textArea.value}");
-    textArea.value = null;
+    _messageStreamController.add(_textArea.value);
+    _textArea.value = null;
   }
 
   /// This method is called when the button "Send" is pressed.
@@ -36,18 +47,18 @@ class TypeMessage extends PolymerElement {
   void inputKeyPressed(CustomEventWrapper event, [_]) {
     _lastKeyWasEnter = false;
     final KeyboardEvent keyboardEvent = event.original as KeyboardEvent;
-    // If a Ctrl and Enter buttons were pressed simultaneously.
     if (keyboardEvent.keyCode == 13 || keyboardEvent.keyCode == 10) {
+      // If a Ctrl and Enter buttons were pressed simultaneously.
       if (keyboardEvent.ctrlKey) {
         // Ctrl+Enter combination transforms into a next line symbol.
-        textArea.value = textArea.value + "\n";
+        _textArea.value = _textArea.value + "\n";
       } else {
         _lastKeyWasEnter = true;
       }
     }
   }
 
-  /// This method is the user types something in the input text area.
+  /// This method is called when the user types something in the input text area.
   @reflectable
   void inputInput(CustomEventWrapper event, [_]) {
     if (_lastKeyWasEnter) {
