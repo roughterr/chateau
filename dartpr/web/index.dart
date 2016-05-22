90f4a1ff7719f9c47e8b93294411dee36ab1951c
roughterr <roughterr@gmail.com>
2016-05-22 12:13:20 +0300

    deleted the main-app element which was for demo purposes

--- dartpr/web/index.dart
+++ dartpr/web/index.dart
@@ -1,11 +1,9 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:dartpr/main_app.dart';
 import 'package:dartpr/message_box/message_box.dart';
 import 'package:polymer/polymer.dart';
 
-/// [MainApp] used!
 /// [MessageBox] used!
 main() async {
   await initPolymer();
