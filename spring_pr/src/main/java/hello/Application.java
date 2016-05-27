46dd827c021c27af176576f59a9bb3a4ab4a5898
roughterr <roughterr@gmail.com>
2016-05-27 10:23:03 +0300

    Moved Application.java to another package because otherwise the application did not work correctly

--- spring_pr/src/main/java/hello/Application.java
+++ spring_pr/src/main/java/hello/Application.java
@@ -1,10 +1,11 @@
-package org.nau.chateau;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * This class starts the server using Spring Boot.
+ * To make it work the class should be in the same package where the other classes of the Spring Boot application are.
  */
 @SpringBootApplication
 public class Application {
