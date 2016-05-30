2621aeb40b35690cccf6c8e59ab05c0f907ac006
roughterr <roughterr@gmail.com>
2016-05-30 12:46:57 +0300

    Added a polymer element that uses STOMP JS

--- dartpr/lib/message_box/message_box.dart
+++ dartpr/lib/message_box/message_box.dart
@@ -25,20 +25,28 @@ class MessageBox extends PolymerElement {
 
   MessageBox.created() : super.created();
 
+  /// This value of this variable will be send to the component that
+  /// communicates with the STOMP server (implementation may be substituted).
+  var messagetosend;
+
   void ready() {
+    print("messagestream is ${messagestream}");
+    this.messagetosend = "1";
+    this.messagetosend = "2";
     //Opens WebSocket connection.
     // ws = new WebSocket('ws://localhost:8080/nauchat/echo');
     //  _streamSubscription =
-    //  messagestream.listen((s) => ws.send("Message from chat: " + s));
-    try {
-      Future<StompClient> stompFuture =
-      connect("ws://localhost:8080/chateau", login : "ian", passcode: "ian", host : "localhost");
-      StompClient stompClient;
-      stompFuture.then((StompClient stompClient) => _doClient(stompClient));
-    } catch (exception, stackTrace) {
-      print(exception);
-      print(stackTrace);
-    }
+//      messagestream.listen((s) => print("Message from chat: " + s));
+    messagestream.listen((s) => set("messagetosend", s));
+//    try {
+//      Future<StompClient> stompFuture =
+//      connect("ws://localhost:8080/chateau", login : "ian", passcode: "ian", host : "localhost");
+//      StompClient stompClient;
+//      stompFuture.then((StompClient stompClient) => _doClient(stompClient));
+//    } catch (exception, stackTrace) {
+//      print(exception);
+//      print(stackTrace);
+//    }
   }
 
   void _doClient(StompClient stompClient) {
