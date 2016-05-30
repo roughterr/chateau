e667d94ecc55525de9b756c7e566d91d7bb45c49
roughterr <roughterr@gmail.com>
2016-05-30 11:58:30 +0300

    Added Javadocs to the spring websocket config.

--- spring_pr/src/main/java/hello/WebSocketConfig.java
+++ spring_pr/src/main/java/hello/WebSocketConfig.java
@@ -9,16 +9,22 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        //It simply allows destinations. No changins paths.
+        config.enableSimpleBroker("/topic", "/commonchat-outcoming");
+        //This address is used when a client wants to message to the server.
+        //It's like the client sends to /app/hello but the server sees it as just /hello
         config.setApplicationDestinationPrefixes("/app");
     }
 
+    /**
+     * Sets an address that will be used when a client is connecting to the STOMP server.
+     *
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/hello").withSockJS();
     }
-
 }
