230a1681c92b0d5b3a99b1ac3db0772fb0211270
romanp <unknown@ksf>
2017-04-18 08:40:29 +0300

    class renamings.

--- ng-client/src/app/services/messaging.service.ts
+++ ng-client/src/app/services/messaging.service.ts
@@ -29,68 +29,88 @@ export class MessagingService {
 
   constructor() {
     this.ws.onmessage = msg => {
-      console.log(`message received. msg.data=${msg.data}`);
+      console.log(`message received. data=${msg.data}`);
       const messageMap = JSON.parse(msg.data);
+      // get a frame type
+      const frameType = messageMap[MessagingService.FRAME_PARAM_NAME];
+      if (frameType == null || frameType === '') {
+        console.log('Server sent a message without a frame name.');
+        return;
+      }
       const destination: string = messageMap[MessagingService.DESTINATION_PARAM_NAME];
       if (destination == null || destination === '') {
         console.log('Server sent a message without a destination.');
         return;
       }
-      // get a channel from the message
-      let channel = messageMap[MessagingService.CHANNEL_PARAM_NAME];
-      if (channel == null || channel === '') {
-        channel = 0;
+      // get a channel index from the message
+      let channelIndex: number = messageMap[MessagingService.CHANNEL_PARAM_NAME];
+      if (channelIndex == null) {
+        channelIndex = 0;
       }
-      let messageID = messageMap[MessagingService.MESSAGEID_PARAM_NAME];
-      if (messageID == null || messageID === '') {
+      let messageID: number = messageMap[MessagingService.MESSAGEID_PARAM_NAME];
+      if (messageID == null) {
         messageID = 0;
       }
-      // get a frame name
-      const frame = messageMap[MessagingService.FRAME_PARAM_NAME];
-      if (frame == null || frame === '') {
-        console.log('Server sent a message without a frame name.');
-        return;
-      }
-      if (frame === MessagingService.CLEANED_FRAME_NAME) {
-        const channelObj: Channel = this.sentDestinations.getDestination(destination).getChannelByIndex(channel);
-        channelObj.waitingList = new Map();
-        channelObj.slowpokePackages = [];
-        channelObj.lastMessageID = -1;
-      } else if (frame === MessagingService.NACK_FRAME_NAME) {// does nothing at the moment
-      } else if (frame === MessagingService.ACK_FRAME_NAME) {// message that is a response to a client's message.
-        const destinationObj: Destination = this.sentDestinations.getDestination(destination);
-        const channelObj = destinationObj.getChannelByIndex(channel);
-        const currentChannelObj = destinationObj.getCurrentChannel();
-        const messageObj = channelObj.waitingList.get(messageID);
-        // cleansing start.
-        messageMap[MessagingService.FRAME_PARAM_NAME] = null;
-        messageMap[MessagingService.MESSAGEID_PARAM_NAME] = null;
-        messageMap[MessagingService.CHANNEL_PARAM_NAME] = null;
-        messageMap[MessagingService.DESTINATION_PARAM_NAME] = null;
-        // cleansing end.
-        messageObj.markAsAcknowledged(messageMap);
-        channelObj.waitingList.delete(messageID);
-        const notCurrentChannel: Channel = destinationObj.getNotCurrentChannel();
-        const theOtherChannelIndex: number = Destination.getTheOtherChannelIndex(channel);
-        const theOtherChannelObj = destinationObj.getChannelByIndex(theOtherChannelIndex);
-        if (theOtherChannelObj.lastMessageID === -1) {
-          destinationObj.switchCurrentChannel();
-        }
-        // if the other channel already active and the current channel's waiting list is empty.
-        if (channel !== destinationObj.currentChannel && channelObj.waitingList.size === 0) {
-          this.requestChannelClean(destination, channel);
-        }
-        // clean the other channel if necessary
-        if (theOtherChannelObj.waitingList.size === 0 && theOtherChannelObj.lastMessageID > -1) {
-          this.requestChannelClean(destination, theOtherChannelIndex);
-        }
-      }
+      // cleansing start.
+      messageMap[MessagingService.FRAME_PARAM_NAME] = null;
+      messageMap[MessagingService.MESSAGEID_PARAM_NAME] = null;
+      messageMap[MessagingService.CHANNEL_PARAM_NAME] = null;
+      messageMap[MessagingService.DESTINATION_PARAM_NAME] = null;
+      // cleansing end.
+      this.processFrame(frameType, destination, channelIndex, messageID, messageMap);
     };
+  }
+
+  /**
+   * Processes an incoming frame.
+   * @param frameType frame type. For example, ACK which says that the frame is an acknowledge about a received message
+   * @param destination destination
+   * @param channelIndex channe index. Possible values are: 0, 1
+   * @param messageID optional. message ID
+   * @param messageMap message data in a form of map
+   */
+  private processFrame(frameType: string, destination: string, channelIndex: number, messageID: number, messageMap) {
+    if (frameType === MessagingService.CLEANED_FRAME_NAME) {
+      const channelObj: SentChannel = this.sentDestinations.getDestination(destination).getChannelByIndex(channelIndex);
+      channelObj.waitingList = new Map();
+      channelObj.slowpokePackages = [];
+      channelObj.lastMessageID = -1;
+    } else if (frameType === MessagingService.NACK_FRAME_NAME) {// does nothing at the moment
+    } else if (frameType === MessagingService.ACK_FRAME_NAME) {// message that is a response to a client's message.
+      const destinationObj: SentDestination = this.sentDestinations.getDestination(destination);
+      const channelObj = destinationObj.getChannelByIndex(channelIndex);
+      const messageObj = channelObj.waitingList.get(messageID);
+      messageObj.markAsAcknowledged(messageMap);
+      channelObj.waitingList.delete(messageID);
+      const theOtherChannelIndex: number = SentDestination.getTheOtherChannelIndex(channelIndex);
+      const theOtherChannelObj = destinationObj.getChannelByIndex(theOtherChannelIndex);
+      if (theOtherChannelObj.lastMessageID === -1) {
+        destinationObj.switchCurrentChannel();
+      }
+      // if the other channel already active and the current channel's waiting list is empty.
+      if (channelIndex !== destinationObj.currentChannel && channelObj.waitingList.size === 0) {
+        this.requestChannelClean(destination, channelIndex);
+      }
+      // clean the other channel if necessary
+      if (theOtherChannelObj.waitingList.size === 0 && theOtherChannelObj.lastMessageID > -1) {
+        this.requestChannelClean(destination, theOtherChannelIndex);
+      }
+    }
+  }
+
+  /**
+   * Sends a frame to the server.
+   * @param frameMap frame map
+   */
+  private sendFrame(frameMap) {
+    const json: string = JSON.stringify(frameMap);
+    console.log(`JSON that is going to be sent to the server: ${json}`);
+    this.ws.send(json);
   }
 
   private requestChannelClean(destination: string, channelIndex: number) {
     // get the channel object
-    const destinationObj: Destination = this.sentDestinations.getDestination(destination);
+    const destinationObj: SentDestination = this.sentDestinations.getDestination(destination);
     const channelObj = destinationObj.getCurrentChannel();
     const cleanFrameMap = {};
     cleanFrameMap[MessagingService.FRAME_PARAM_NAME] = MessagingService.CLEAN_FRAME_NAME;
@@ -104,11 +124,11 @@ export class MessagingService {
         channelObj.slowpokePackages.fill(messageObj);
       }
     }, 5000);
-    this.ws.send(JSON.stringify(cleanFrameMap));
+    this.sendFrame(cleanFrameMap);
   }
 
   sendMessage(destination: string, data): SentMessage {
-    const destinationObj: Destination = this.sentDestinations.getDestination(destination);
+    const destinationObj: SentDestination = this.sentDestinations.getDestination(destination);
     const channelObj = destinationObj.getCurrentChannel();
     channelObj.lastMessageID++;
     data[MessagingService.FRAME_PARAM_NAME] = MessagingService.SEND_FRAME_NAME;
@@ -116,10 +136,8 @@ export class MessagingService {
     data[MessagingService.DESTINATION_PARAM_NAME] = destination;
     data[MessagingService.MESSAGEID_PARAM_NAME] = channelObj.lastMessageID;
     const sentMessage: SentMessage = new SentMessage(data);
-    const json: string = JSON.stringify(data);
-    console.log(`JSON that is going to be sent to the server: ${json}`);
     channelObj.waitingList.set(channelObj.lastMessageID, sentMessage);
-    this.ws.send(json);
+    this.sendFrame(data);
     return sentMessage;
     // TODO timeout and add an item slowpokePackages
   }
@@ -143,7 +161,10 @@ class SentMessage {
   }
 }
 
-class Channel {
+/**
+ * Contains data about frames sent to a channel.
+ */
+class SentChannel {
   waitingList: Map<number, SentMessage> = new Map();
   /**
    * List of packages that we are supposed to have already received acknowledgement about.
@@ -157,11 +178,14 @@ class Channel {
   lastMessageID = -1;
 }
 
-class Destination {
+/**
+ * Contains data about frames sent to destinations.
+ */
+class SentDestination {
   /** An object with data of the first channel. */
-  firstChannel: Channel = new Channel();
+  firstChannel: SentChannel = new SentChannel();
   /** An object with data of the second channel. */
-  secondChannel: Channel = new Channel();
+  secondChannel: SentChannel = new SentChannel();
   /** ID of the current channel. Possible values are: 0, 1. */
   currentChannel = 0;
 
@@ -169,24 +193,8 @@ class Destination {
     return index === 0 ? 1 : 0;
   }
 
-  getCurrentChannel(): Channel {
+  getCurrentChannel(): SentChannel {
     return this.currentChannel === 0 ? this.firstChannel : this.secondChannel;
-  }
-
-  setCurrentChannel(channel: Channel) {
-    if (this.currentChannel === 0) {
-      this.firstChannel = channel;
-    } else {
-      this.secondChannel = channel;
-    }
-  }
-
-  getNotCurrentChannelIndex() {
-    return this.currentChannel === 0 ? 1 : 0;
-  }
-
-  getNotCurrentChannel() {
-    return this.currentChannel === 0 ? this.secondChannel : this.firstChannel;
   }
 
   /**
@@ -202,19 +210,19 @@ class Destination {
 }
 
 /**
- * Contains data about packages sent to destinations.
+ * Contains data about frames sent to destinations.
  */
 class SentDestinations {
   /**
    * Map, where the key is a destination and value is data of the destination.
    */
-  destinations: Map<string, Destination> = new Map();
+  destinations: Map<string, SentDestination> = new Map();
 
-  getDestination(destination: string): Destination {
+  getDestination(destination: string): SentDestination {
     if (this.destinations.has(destination)) {
       return this.destinations.get(destination);
     } else {
-      const destinationObj = new Destination();
+      const destinationObj = new SentDestination();
       this.destinations.set(destination, destinationObj);
       return destinationObj;
     }
