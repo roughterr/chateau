1ddfdab950b7c09c082e4e982a41ac27cccb9031
roughterr <roughterr@gmail.com>
2024-10-08 15:00:55 +0200

    upgraded Spring Boot version from 1.3.5.RELEASE to 3.3.4

--- spring_pr/src/main/java/org/nau/config/SecurityConfig.java
+++ spring_pr/src/main/java/org/nau/config/SecurityConfig.java
@@ -1,60 +1,46 @@
 package org.nau.config;
 
-import org.nau.database.UserDao;
-import org.nau.database.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import java.util.List;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private NauUserDetailService nauUserDetailService;
+public class SecurityConfig {
+	@Autowired
+	private NauUserDetailService nauUserDetailService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() // DISABLED CSRF protection to make it easier !
-                .authorizeRequests()
-                .antMatchers("/*.html").permitAll()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .permitAll()
-                .defaultSuccessUrl("/chateau.html")
-                .and()
-                .logout()
-                .permitAll()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/");
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/*.html").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin(Customizer.withDefaults())
+			.logout(logout -> logout.logoutUrl("/logout").permitAll().logoutSuccessUrl("/"));
+		return http.build();
+	}
 
-        http.sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry());
-    }
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(nauUserDetailService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
+	}
 
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder builder)
-            throws Exception {
-        builder.userDetailsService(nauUserDetailService);
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
